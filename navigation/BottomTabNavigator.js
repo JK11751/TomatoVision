d0cd0f51399c6f06a04895c3f825dcb517073be9
JK11751 <kgimase@gmail.com>
2024-08-28 08:31:42 +0300

    apk generated

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,108 +1,79 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import * as React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as React from "react";
 
-import TabBarIcon from '../components/TabBarIcon';
-import { FontAwesome } from '@expo/vector-icons';
-import TabBarLabel from '../components/TabBarLabel';
-import HomeScreen from '../screens/HomeScreen';
-import DiseaseListScreen from '../screens/DiseaseListScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import { Ionicons } from '@expo/vector-icons';
-import { COLORS } from '../constants/Colors';
+import TabBarIcon from "../components/TabBarIcon";
+import { FontAwesome } from "@expo/vector-icons";
+import TabBarLabel from "../components/TabBarLabel";
+import HomeScreen from "../screens/HomeScreen";
+import DiseaseListScreen from "../screens/DiseaseListScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../constants/Colors";
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = "Home";
 
 export default function CustomerBottomTabNavigator() {
-  
-
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          title: "",
+          tabBarLabel: "Home",
+          tabBarHideOnKeyboard: true,
+          tabBarActiveTintColor: COLORS.primary,
+          tabBarInactiveTintColor: COLORS.black,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={28}
+              color={focused ? COLORS.primary : COLORS.gray2}
+            />
+          ),
+        }}
+      />
 
-
-<BottomTab.Screen
-         name='Home'
-         component={HomeScreen}
-          options={{
-            headerShown: false,
-            title: '',
-            tabBarLabel: 'Home',
-            tabBarHideOnKeyboard: true,
-            tabBarActiveTintColor: COLORS.primary,
-            tabBarInactiveTintColor: COLORS.black,
-            tabBarIcon: ({focused}) => (
+      <BottomTab.Screen
+        name="Diseases"
+        component={DiseaseListScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: "My plants",
+          tabBarActiveTintColor: COLORS.primary,
+          tabBarInactiveTintColor: COLORS.black,
+          tabBarIcon: ({ focused, color, position }) => {
+            return (
               <Ionicons
-                name={focused ? 'home' : 'home-outline'}
+                name={focused ? "leaf" : "leaf-outline"}
                 size={28}
                 color={focused ? COLORS.primary : COLORS.gray2}
               />
-            ),
-          }}
-        />
-      
-         <BottomTab.Screen
-         name='Diseases'
-         component={DiseaseListScreen}
-          options={{
-            headerShown: false,
-            tabBarLabel: 'My plants',
-            tabBarActiveTintColor: COLORS.primary,
-            tabBarInactiveTintColor: COLORS.black,
-            tabBarIcon: ({focused, color, position}) => {
-              return (
-                <Ionicons
-                  name={focused ? 'leaf' : 'leaf-outline'}
-                  size={28}
-                  color={focused ? COLORS.primary : COLORS.gray2}
-                />
-              );
-            },
-          }}
-        /> 
-        <BottomTab.Screen
-          name='Profile'
-          component={ProfileScreen}
-          options={{
-            headerShown: false,
-            tabBarLabel: 'My account',
-            tabBarHideOnKeyboard: true,
-            tabBarActiveTintColor: COLORS.primary,
-            tabBarInactiveTintColor: COLORS.black,
-            tabBarIcon: ({focused}) => {
-              return (
-                <FontAwesome
-                  name={focused ? 'user-circle' : 'user-circle-o'}
-                  size={28}
-                  color={focused ? COLORS.primary : COLORS.gray2}
-                />
-              );
-            },
-          }}
-        />
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            );
+          },
+        }}
+      />
+      <BottomTab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: "My account",
+          tabBarHideOnKeyboard: true,
+          tabBarActiveTintColor: COLORS.primary,
+          tabBarInactiveTintColor: COLORS.black,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <FontAwesome
+                name={focused ? "user-circle" : "user-circle-o"}
+                size={28}
+                color={focused ? COLORS.primary : COLORS.gray2}
+              />
+            );
+          },
+        }}
+      />
 
       {/* <BottomTab.Screen
         name='Home'
